f4306e093dbb6175a7c405126b6b6c5381831afd
fabricio-cesur <fabricio.s399523@cesurformacion.com>
2025-10-09 03:41:10 +0200

    FEAT: Jump Input Buffer
    The jump felt strange and required a lot of accuracy and timing to get
    the jump right, so it obviously needed an input buffer for the jump. The
    jump script was reworked with the implementation of an input buffer, as
    tested the jump now feels better but there is still problems with the
    horizontal collissions.

--- Assets/Scripts/Player/Jump.cs
+++ Assets/Scripts/Player/Jump.cs
@@ -1,32 +1,63 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
 
     public float force = 13f;
+    public float input_buffer_time = 0.1f;
     private Rigidbody2D rb;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Queue<KeyCode> input_buffer; // queue to save the inputs of the jump
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        input_buffer = new Queue<KeyCode>();
     }
 
     private bool grounded = true;
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) && grounded)
+        // if you press W it will be saved on the queue
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            rb.AddForce(Vector3.up * force, ForceMode2D.Impulse);
-            grounded = false;
+            input_buffer.Enqueue(KeyCode.W);
+            Invoke("removeAction", input_buffer_time);
         }
-    }
 
+        // if you're touching ground
+        if (grounded)
+        {
+            // if the input buffer has more than 1 key pressed saved
+            if (input_buffer.Count > 0)
+            {
+                // if that next pressed key is the jump key
+                if (input_buffer.Peek() == KeyCode.W)
+                {
+                    // object jumps
+                    rb.AddForce(Vector3.up * force, ForceMode2D.Impulse);
+                    // and it's not touching the ground anymore
+                    grounded = false;
+
+                    // removing the action from the queue
+                    input_buffer.Dequeue();
+                }
+            }
+        }
+
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("ground"))
         {
             grounded = true;
+        }
+    }
+
+    void removeAction()
+    {   
+        if (input_buffer.Count > 0)
+        {
+            input_buffer.Dequeue();
         }
     }
 }
