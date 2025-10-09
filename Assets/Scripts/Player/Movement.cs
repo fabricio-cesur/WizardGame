560c6ac2cf8c860494e790befb230dfadf1653af
fabricio-cesur <fabricio.s399523@cesurformacion.com>
2025-10-09 13:16:01 +0200

    CHANGE: Translate to Velocity
    The primitive movement using transform.translate was crating glitches
    with collissions, so I changed from changing the position to change the
    velocity using the Rigidbody2D.

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -2,32 +2,35 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 10f;            
-    
+    public float speed = 10f;
+    private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 direction = Vector2.zero;
+        // float variable which will indicate what change to do to the position X
+        // if it's 0 then no change will be applied to the X position
+        float horizontal_direction = 0f;
 
         //Horizontal Movement
-        if (Input.GetKey(KeyCode.A)) 
-        { 
-            direction += Vector2.left; 
+        if (Input.GetKey(KeyCode.A))
+        {
+            // subtract to X position each frame
+            horizontal_direction = -1f; 
         }
-        if (Input.GetKey(KeyCode.D)) 
-        { 
-            direction += Vector2.right; 
+        if (Input.GetKey(KeyCode.D))
+        {
+            // add to X position each frame
+            horizontal_direction = 1f;
         }
 
-        if (direction != Vector2.zero)
-        {
-            transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
-        }
+        // setting velocity to object directly
+        rb.linearVelocity = new Vector2(horizontal_direction * speed, rb.linearVelocity.y);
+        
     }
 }
