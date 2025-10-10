0c8b467ae15d51f58be3c6b2a655814ec08e2eef
fabricio-cesur <fabricio.s399523@cesurformacion.com>
2025-10-10 15:25:04 +0200

    FEAT: DeathZone and Staff
    I made a DeathZone that will reload the scene if the player touches it,
    it has some delay between the collission and the reload. And also made a
    Staff object with a rectangle and added it a new script that will rotate
    that object following the mouse.

--- Assets/Scripts/Player/Die.cs
+++ Assets/Scripts/Player/Die.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Die : MonoBehaviour
 {
@@ -12,5 +13,17 @@ public class Die : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("DeathZone"))
+        {
+            restart();
+        }
+    }
+
+    private void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
---
+++ Assets/Scripts/Player/LAMouse.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class LAMouse : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePos - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+    }
+}
