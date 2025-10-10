9b622cc544d9cd48de75a7dbc37b612ddfb43666
fabricio-cesur <fabricio.s399523@cesurformacion.com>
2025-10-10 15:36:21 +0200

    CHANGE: Variable names and commented
    I changed the variable names to make sense with the personal variable
    name convention. Also added some comments to better explain the
    functioning of the script and how it gets the angle needed to point at
    the mouse.

--- Assets/Scripts/Player/LAMouse.cs
+++ Assets/Scripts/Player/LAMouse.cs
@@ -11,9 +11,16 @@ public class LAMouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = mousePos - transform.position;
+        // get the position of the mouse in screen
+        Vector3 mouse_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        // get the vector as the stepts to get to the position of the mouse
+        Vector2 direction = mouse_position - transform.position;
+
+        // Atan2 creates a radian with the positions, and then with Rad2Deg it converts to degrees
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // we change the object's rotation according to the angle given
         transform.rotation = Quaternion.Euler(0, 0, angle - 90);
     }
 }
