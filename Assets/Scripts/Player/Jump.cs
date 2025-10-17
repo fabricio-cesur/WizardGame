73bce82ef3385af78877e3b0f5bdef61dd4735c0
fabricio-cesur <fabricio.s399523@cesurformacion.com>
2025-10-17 09:12:02 +0200

    FEATURE: Coyote Time and tweaks on jump
    I added coyote time so that the player is able to jump some miliseconds
    after falling from the edge of a platform. Also I changed the force of
    the jump to 20 and the gravity scale to 6, I think the jump feels better
    as it is not much higher but it falls down faster.

--- Assets/Scripts/Player/Jump.cs
+++ Assets/Scripts/Player/Jump.cs
@@ -6,7 +6,10 @@ public class Jump : MonoBehaviour
 
     public float force = 13f;
     private Rigidbody2D rb;
-    
+
+    private bool jumping;
+    public float coyote_time = 0.18f;
+    private float coyote_time_counter;
     public float buffer_time = 0.15f;
     private float buffer_time_counter;
 
@@ -14,6 +17,9 @@ public class Jump : MonoBehaviour
     private float player_half_height;
     private float player_half_width;
     private float center_raycast_ground_length;
+    private float ray_offset = 0.01f;
+
+    private LayerMask ground_layermask;
     void Start()
     {
         // getting rigidbody of object
@@ -25,49 +31,65 @@ public class Jump : MonoBehaviour
         // distance between the center of the sprite and the vertical border
         player_half_height = sr.bounds.extents.y;
         // we add an extra .1 to that distance so that it touches the ground slightly
-        center_raycast_ground_length = player_half_height + 0.1f;
+        center_raycast_ground_length = player_half_height + 0.05f;
+        ground_layermask = LayerMask.GetMask("Ground");
     }
 
     void Update()
     {
+
+        if (isGrounded())
+        {
+            coyote_time_counter = coyote_time;
+        }
+        else
+        {
+            coyote_time_counter -= Time.deltaTime;
+        }
+
+        if (isFalling() && jumping)
+        {
+            jumping = false;
+        }
+
         // press W and the counter will be set to the according value
         if (Input.GetKeyDown(KeyCode.W))
         {
             buffer_time_counter = buffer_time;
         }
-
-        // if the counter is greater than 0 (which will be the case if it was set to the value)
-        // and if it is on the ground it will jump
-        if (buffer_time_counter > 0f && isGrounded())
-        {
-            jump();
-            // the counter will be set to 0 because it just jumped
-            buffer_time_counter = 0f;
-        }
         else
         {
-            // if not we start taking from the counter so that
-            // the window to jump only last as long as you stated
             buffer_time_counter -= Time.deltaTime;
         }
 
+        // if the counter is greater than 0 (which will be the case if it was set to the value)
+        // and if it is on the ground it will jump
+        if (coyote_time_counter > 0f && buffer_time_counter > 0f && !jumping)
+        {
+            jumping = true;
+            jump();
+            // the counter will be set to 0 because it just jumped
+            buffer_time_counter = 0f;
+            coyote_time_counter = 0f;
+        }
+
         // drawing the raycast for debugging
-        Debug.DrawRay(new Vector2(transform.position.x + player_half_width, transform.position.y), Vector2.down * center_raycast_ground_length);
-        Debug.DrawRay(transform.position, Vector2.down * center_raycast_ground_length);
-        Debug.DrawRay(new Vector2(transform.position.x - player_half_width, transform.position.y), Vector2.down * center_raycast_ground_length);
+        Debug.DrawRay(new Vector2(transform.position.x - player_half_width + ray_offset, transform.position.y), Vector2.down * center_raycast_ground_length, Color.red);
+        Debug.DrawRay(transform.position, Vector2.down * center_raycast_ground_length, Color.red);
+        Debug.DrawRay(new Vector2(transform.position.x + player_half_width - ray_offset, transform.position.y), Vector2.down * center_raycast_ground_length, Color.red);
     }
 
     private bool isGrounded()
     {
         // origin points for the raycasts
         Vector2 center = transform.position;
-        Vector2 left = new Vector2(center.x - player_half_width, center.y);
-        Vector2 right = new Vector2(center.x + player_half_width, center.y);
+        Vector2 left = new Vector2(center.x - player_half_width + ray_offset, center.y);
+        Vector2 right = new Vector2(center.x + player_half_width - ray_offset, center.y);
 
         // check if the raycast are touching ground
-        return Physics2D.Raycast(left, Vector2.down, center_raycast_ground_length, LayerMask.GetMask("Ground"))
-            || Physics2D.Raycast(center, Vector2.down, center_raycast_ground_length, LayerMask.GetMask("Ground"))
-            || Physics2D.Raycast(right, Vector2.down, center_raycast_ground_length, LayerMask.GetMask("Ground"));
+        return Physics2D.Raycast(left, Vector2.down, center_raycast_ground_length, ground_layermask)
+            || Physics2D.Raycast(center, Vector2.down, center_raycast_ground_length, ground_layermask)
+            || Physics2D.Raycast(right, Vector2.down, center_raycast_ground_length, ground_layermask);
     }
 
     void jump()
@@ -77,5 +99,10 @@ public class Jump : MonoBehaviour
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
         // then the vertical up force is added to the object
         rb.AddForce(Vector3.up * force, ForceMode2D.Impulse);
+    }
+
+    private bool isFalling()
+    {
+        return rb.linearVelocity.y < 0f;
     }
 }
