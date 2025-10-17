f011ad6239a2b478e8c1d221cf5c1625a2e6254e
fabricio-cesur <fabricio.s399523@cesurformacion.com>
2025-10-17 14:20:55 +0200

    ADD: Setup for the Health System

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -1,24 +1,33 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Die : MonoBehaviour
+public class Health : MonoBehaviour
 {
+    public int hp = 3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("DeathZone"))
+        if (hp <= 0)
         {
             restart();
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            hp--;
+        }
+        if (collision.gameObject.CompareTag("DeathZone"))
+        {
+            hp = 0;
         }
     }
 
